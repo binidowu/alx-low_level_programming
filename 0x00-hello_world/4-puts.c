ef4a17ca60852d68866e5f60a6e66535cce71b4f
Adebayo Ayomide <adebayormide@gmail.com>
2022-06-30 09:18:51 -0700

    Task: 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Writes out the value of char variable to stdout using puts()
+ * @char str: Stores the value of str to be printed out
+ *
+ * Return: Prints outs str value
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+
+	return (0);
+}
