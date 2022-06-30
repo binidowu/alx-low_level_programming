d4a7c60c0d23fa4466e9237742ff6b20ce148af0
Adebayo Ayomide <adebayormide@gmail.com>
2022-06-30 12:15:11 -0700

    Task: 101-quote.c v1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Writes string to stderr without using puts or printf
  *
- * Return: Prints out string using writes
+ * Return: Prints out string using writes. Writes 1 for success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
