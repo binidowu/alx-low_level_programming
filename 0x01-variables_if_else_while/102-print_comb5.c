6a7369a8d85a14264d5e474136c7747f7027121b
Adebayo Ayomide <adebayormide@gmail.com>
2022-07-01 16:32:24 -0700

    Task: 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -> Write all  possible combinations of two two-digit numbers.
+ *
+ * Return: Two two-digit numbers
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = 0; b <= 99; b++)
+		{
+			if (b > a)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
