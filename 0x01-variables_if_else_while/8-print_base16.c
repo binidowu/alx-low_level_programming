0e527edb345d248486ca953ae9597857562e61e4
Adebayo Ayomide <adebayormide@gmail.com>
2022-07-01 13:39:51 -0700

    Task: 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Write all numbers of base 16 in lowercase to stdout
+ *
+ * Return: Base16 numbers
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
