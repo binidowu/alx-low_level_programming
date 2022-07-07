93b174799cedbbdd6018af86ecc402b010952b1e
Adebayo Ayomide <adebayormide@gmail.com>
2022-07-07 14:31:51 -0700

    Task: 3-print_numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 /*
- * Auth: Adebayo P. Ayomide
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x03-more_functions_nested_loops directory.
  */
@@ -10,5 +9,7 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
 
 #endif
