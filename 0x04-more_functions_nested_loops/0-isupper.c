a1e2f56073793992d7ff9fb4bf66e475ff7b2975
Adebayo Ayomide <adebayormide@gmail.com>
2022-07-07 13:41:26 -0700

    Task: 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_upper - Checks for uppercase character.
+ * @c: Character to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
+int is_upper(int c)
+{
+	if (c <= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
