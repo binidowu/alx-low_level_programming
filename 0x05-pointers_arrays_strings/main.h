208f12d10de051e92fe3946e49a032dfb558201e
Adebayo Ayomide <adebayormide@gmail.com>
2022-07-12 12:33:40 -0700

    Task: 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
